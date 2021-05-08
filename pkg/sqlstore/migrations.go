05b1ea88d6de4a50e2a6172b500a3a45c5bcc8d6
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-05-08 16:00:54 +0300

    sql store migration template added

--- pkg/sqlstore/migrations.go
+++ pkg/sqlstore/migrations.go
@@ -3,7 +3,6 @@ package sqlstore
 import (
 	"github.com/blang/semver"
 	"github.com/jmoiron/sqlx"
-	"github.com/pkg/errors"
 )
 
 type Migration struct {
@@ -19,11 +18,6 @@ var migrations = []Migration{
 		fromVersion: semver.MustParse("0.0.0"),
 		toVersion:   semver.MustParse("0.1.0"),
 		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
-			if _, err := e.Exec(`
-				
-			`); err != nil {
-				return errors.Wrapf(err, "failed creating table")
-			}
 
 			return nil
 		},
