ac3c449af36b77be7d8c71223f59e0e1eda7537c
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-05-08 16:36:10 +0300

    sql store updates

--- pkg/sqlstore/store.go
+++ pkg/sqlstore/store.go
@@ -1,8 +1,6 @@
 package sqlstore
 
 import (
-	"strings"
-
 	sq "github.com/Masterminds/squirrel"
 	"github.com/influxdata/influxdb/kit/errors"
 	"github.com/jmoiron/sqlx"
@@ -40,36 +38,4 @@ func New(cfg Config) (*SQLStore, error) {
 		db:      db,
 		builder: builder,
 	}, nil
-}
-
-// parseDSN splits up a connection string into a driver name and data source name.
-//
-// For example:
-//	mysql://gojordomo:pass@localhost:5432/gojordomo
-// returns
-//	driverName = mysql
-//	dataSourceName = gojordomo:pass@localhost:5432/gojordomo
-//
-// By contrast, a Postgres DSN is returned unmodified.
-func parseDSN(dsn string) (string, string, error) {
-	// Treat the DSN as the URL that it is.
-	s := strings.SplitN(dsn, "://", 2)
-	if len(s) != 2 {
-		return "", "", errors.New("failed to parse DSN as URL")
-	}
-
-	scheme := s[0]
-	switch scheme {
-	case "mysql":
-		// Strip off the mysql:// for the dsn with which to connect.
-		dsn = s[1]
-
-	case "postgres":
-		// No changes required
-
-	default:
-		return "", "", errors.Errorf("unsupported scheme %s", scheme)
-	}
-
-	return scheme, dsn, nil
 }
