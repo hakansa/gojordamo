a9031c75a12ebfb97caf22fe0e80373bad725b55
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-05-08 15:59:59 +0300

    sql store migration template added

--- pkg/sqlstore/migrations.go
+++ pkg/sqlstore/migrations.go
@@ -20,12 +20,9 @@ var migrations = []Migration{
 		toVersion:   semver.MustParse("0.1.0"),
 		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
 			if _, err := e.Exec(`
-				CREATE TABLE IF NOT EXISTS IR_System (
-					SKey VARCHAR(64) PRIMARY KEY,
-					SValue VARCHAR(1024) NULL
-				);
+				
 			`); err != nil {
-				return errors.Wrapf(err, "failed creating table IR_System")
+				return errors.Wrapf(err, "failed creating table")
 			}
 
 			return nil
