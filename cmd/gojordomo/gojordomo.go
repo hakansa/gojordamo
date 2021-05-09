2612e75537da26ef305f656e78b3269d8d1b0f6d
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-05-09 21:10:11 +0300

    init func additions

--- cmd/gojordomo/gojordomo.go
+++ cmd/gojordomo/gojordomo.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	configFileFlag = flag.String("config.file", "config.yml", "Path to the configuration file.")
+	logFileFlag    = flag.String("log.file", "license_controller.log", "Path to the log file.")
+	versionFlag    = flag.Bool("version", false, "Show version information.")
+	debugFlag      = flag.Bool("debug", false, "Show debug information.")
+)
+
+func init() {
+	// Parse command-line flags
+	flag.Parse()
+
+	// Log settings
+	if *debugFlag {
+		logrus.SetReportCaller(true)
+		logrus.SetLevel(logrus.TraceLevel)
+	} else {
+		logrus.SetReportCaller(false)
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	logFile, err := os.OpenFile(*logFileFlag, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not open log file")
+	}
+
+	logrus.SetOutput(logFile)
+}
+
+func main() {
+
+}
